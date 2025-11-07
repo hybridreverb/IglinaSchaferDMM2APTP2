e10916f27f1a90131a4606d1ad8b87895dbdcb77
hybridreverb <franco.iglina@davinci.edu.ar>
2025-11-07 20:12:52 -0300

    php cambio

--- enviar_formulario.php
+++ enviar_formulario.php
@@ -1,47 +1,26 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if (isset($_POST["enviar"])){
+    $nombre=$_POST["nombre"];
+    $apellido=$_POST["apellido"];
+    $email=$_POST["email"];
+    $mensaje=$_POST["mensaje"];
 
-    $nombre = htmlspecialchars(trim($_POST["name"]));
-    $apellido = htmlspecialchars(trim($_POST["surname"]));
-    $email = htmlspecialchars(trim($_POST["email"]));
-    $mensaje = htmlspecialchars(trim($_POST["mensaje"]));
+    $destinatario="franco.iglina@gmail.com";
+    $asunto="Nuevo mensaje de $email";
 
-    if (empty($nombre) || empty($apellido) || empty($email) || empty($mensaje)) {
-        echo "<h3 style='color:red;'>Por favor, complete todos los campos.</h3>";
-        exit;
+    $contenido="Nombre: $nombre \n";
+    $contenido="Apellido: $apellido \n";
+    $contenido.="Email: $email \n";
+    $contenido.="Mensaje: $mensaje";
+
+    $header="From: housemd@davinci.com";
+
+    $mail=mail($destinatario,$asunto,$contenido,$header);
+
+    if($mail){
+        echo "<script>alert('El correo se envio correctamente');</script>";
+    }else{
+        echo "<script>alert('El correo no se envio');</script>";
     }
-
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo "<h3 style='color:red;'>El correo electrónico no es válido.</h3>";
-        exit;
-    }
-
-    $destinatario = "franco.iglina@gmail.com";
-    $asunto = "Nuevo mensaje del formulario de contacto";
-
-    $cuerpo = "
-    Has recibido un nuevo mensaje desde el formulario web:
-    ------------------------------------
-    Nombre: $nombre
-    Apellido: $apellido
-    Email: $email
-    Mensaje:
-    $mensaje
-    ------------------------------------
-    Enviado el " . date("d/m/Y H:i") . "
-    ";
-
-    $cabeceras = "From: $email\r\n";
-    $cabeceras .= "Reply-To: $email\r\n";
-    $cabeceras .= "X-Mailer: PHP/" . phpversion();
-
-    if (mail($destinatario, $asunto, $cuerpo, $cabeceras)) {
-        echo "<h3 style='color:green;'>¡Gracias $nombre! Tu mensaje ha sido enviado correctamente.</h3>";
-    } else {
-        echo "<h3 style='color:red;'>Hubo un error al enviar el mensaje. Inténtalo nuevamente más tarde.</h3>";
-    }
-
-} else {
-    echo "<h3 style='color:red;'>Acceso no permitido.</h3>";
 }
 ?>
